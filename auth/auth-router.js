4e5f9f2fdc6e80932cc8b6ac38319ec6cdc5b443
Mike VanSleen <mike.vansleen@gmail.com>
2019-08-23 09:40:04 -0600

    Login and register are functional

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -27,7 +27,7 @@ router.post("/login", (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user && bcrypt.comepareSync(password, user.password)) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user);
         res.status(200).json({
           message: `Welcome ${user.username}!`,
